ac9787b40795e738d9d21ad32dff8d5301896395
John McMillan <john@mcmillantech.co.uk>
2020-01-30 14:43:27 +0000

    V1

---
+++ customerpost.php
@@ -0,0 +1,44 @@
+<?php
+
+// -------------------------------------------------------------
+//  Project	Gallery Build
+//  File	Customerpost.php
+//
+//  Author	John McMillan
+//              McMillan Technology 
+//
+//  Posted from customer form. Calls Customer methods to post
+// --------------------------------------------------------------
+require_once "Customer.php";
+require_once 'configuration.php';
+
+//echo "Customer post<br>";
+
+$customer = new Customer();
+$config = new Configuration;
+
+$action = $_GET['action'];
+
+switch ($action) {
+    case "insert":
+        $customer->insert();
+        $config->insert();
+        break;
+    case "update":
+        $customer->update(1);
+        break;
+    case "configedit":
+        $cusId = 1;
+        $config->edit($cusId);
+        break;
+    case "configupdate":
+        $cusId = 1;
+        $config->update($cusId);
+        break;
+        
+}
+
+?>
+
+<br><br>
+<button type="button" onClick="window.location='index.php'">Home</button>
